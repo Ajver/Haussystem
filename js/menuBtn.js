9aac219c9f7d84b341d33665ef4cd369a2bbbe55
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2018-07-16 14:22:37 +0200

    Menu
    Dodałem rozwijane menu

--- js/menuBtn.js
+++ js/menuBtn.js
@@ -1,7 +1,9 @@
 (function() {
 	var menuBtn = $('a.hamburger'),
-	menuBtnBlock = menuBtn.find('div.hamburger-block');  
+	menuBtnBlock = menuBtn.find('div.hamburger-block'),
+	menu = $('.main-nav');
 	menuBtn.on('click', function() {
 		menuBtnBlock.toggleClass('active');
+		menu.slideToggle(200, function(){});
 	});
 })();
