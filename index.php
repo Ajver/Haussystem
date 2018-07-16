664ad9b7386c2ec8b1b2845f3310148a6dbc2e9b
Ajver <zawlockidominik@gmail.com>
2018-07-16 23:21:42 +0200

    Added top and bottom of page content

--- index.php
+++ index.php
@@ -1,53 +1,9 @@
-<!DOCTYPE html>
-<html lang="pl">
-
-<head>
-  <meta charset="UTF-8">
-  <title>O nas - Haussystem</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext" rel="stylesheet">
-  <link rel="shortcut icon" href="img/icon.png?1">
-  <link rel="stylesheet" href="css/style.css?1">
-</head>
-
-<body>
-  <header class="page-header">
-    <div class="logo"><img src="img/HausSystem-logo orange.png" alt="Haussystem"></div>
-    <div class="hamburger">
-      <div class="hamburger-block top"></div>
-      <div class="hamburger-block middle"></div>
-      <div class="hamburger-block bottom"></div>
-    </div>
-    <nav class="main-nav">
-      <a title="Poczytaj czym się zajmujemy" class="menu-btn about-us-btn" href="index.html">O nas</a>
-      <a title="Poznaj naszą ofertę" class="menu-btn offer-btn" href="index.html">Oferta</a>
-      <a title="Skontaktuj się z nami" class="menu-btn contact-btn" href="index.html">Kontakt</a>
-    </nav>
-  </header>
-  <main>
+<?php include('page-top.php') ?>
+   
     <header>
       <h1>O nas</h1>
     </header>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, beatae a! Ipsam explicabo deleniti harum, similique doloribus quam atque. A eligendi, harum laborum esse ipsum nihil adipisci vel sequi voluptate!</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, beatae a! Ipsam explicabo deleniti harum, similique doloribus quam atque. A eligendi, harum laborum esse ipsum nihil adipisci vel sequi voluptate!</p>
-  </main>
-  <footer class="page-footer">
-    <header>
-      <h2>Skontaktuj się z Piotrem:</h2>
-    </header>
-    <p class="contact phone">+48 111 222 333</p>
-    <p class="contact mail">piotr@example.com</p>
-    <h2>Znajdź nas na mapie:</h2>
-    <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10232.168524922956!2d19.94883764602662!3d50.12293357836606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716508455927c7b%3A0xf882512547ab7c!2s32-087+Bibice!5e0!3m2!1spl!2spl!4v1531743478605" frameborder="0" style="border:0;" allowfullscreen></iframe>
-    <aside class="page-author">
-      <p>Made by <a href="#">Zawlocki and Pałka</a></p>
-    </aside>
-  </footer>
 
-  <script src="js/main.js?1" type="text/javascript"></script>
-
-</body>
-
-</html>
+<?php include('page-bottom.php') ?>
---
+++ page-top.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="UTF-8">
+  <title>O nas - Haussystem</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext" rel="stylesheet">
+  <link rel="shortcut icon" href="img/icon.png?1">
+  <link rel="stylesheet" href="css/style.css?1">
+</head>
+
+<body>
+  <header class="page-header">
+    <div class="logo"><img src="img/HausSystem-logo orange.png" alt="Haussystem"></div>
+    <div class="hamburger">
+      <div class="hamburger-block top"></div>
+      <div class="hamburger-block middle"></div>
+      <div class="hamburger-block bottom"></div>
+    </div>
+    <nav class="main-nav">
+      <a title="Poczytaj czym się zajmujemy" class="menu-btn about-us-btn" href="haussystem-o-nas">O nas</a>
+      <a title="Poznaj naszą ofertę" class="menu-btn offer-btn" href="nasza-oferta">Oferta</a>
+      <a title="Skontaktuj się z nami" class="menu-btn contact-btn" href="kontakt-z-nami">Kontakt</a>
+    </nav>
+  </header>
+  <main>
