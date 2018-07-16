e2bef7c57b791754ed9a40a9d9d1273046f2d453
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2018-07-16 20:36:41 +0200

    ...

--- js/menuBtn.js
+++ js/menuBtn.js
@@ -1,8 +1,16 @@
+function delay (URL, time) {
+    setTimeout( function() { window.location = URL }, time );
+}
 (function() {
 	var menuBtn = $('a.hamburger'),
 	menuBtnBlock = menuBtn.find('div.hamburger-block'),
-	menu = $('.main-nav');
+	menu = $('.main-nav'),
+	menuEl = $('.menu-btn');
 	menuBtn.on('click', function() {
+		menuBtnBlock.toggleClass('active');
+		menu.slideToggle(500, function(){});
+	});
+	menuEl.on('click', function(){
 		menuBtnBlock.toggleClass('active');
 		menu.slideToggle(500, function(){});
 	});
