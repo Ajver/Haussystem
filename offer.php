b552bbecc8f83ba5258914deb28328524627b2a1
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2018-07-16 23:48:05 +0200

    Add offerts cards

--- offer.php
+++ offer.php
@@ -1,5 +1,9 @@
 <?php include('page-top.php') ?>
-
-
+	<link rel="stylesheet" href="css/offer.css">
+	
+	<div class="one-offer">Pierwsza oferta</div>
+	<div class="one-offer">Druga oferta</div>
+	<div class="one-offer">Trzecia oferta</div>
+	<div class="one-offer">Czwarta oferta</div>
 
 <?php include('page-bottom.php') ?>
