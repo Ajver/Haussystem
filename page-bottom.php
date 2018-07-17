7bc838ce14cf6616e3746849273dee3bb4e7a43b
Ajver <zawlockidominik@gmail.com>
2018-07-17 23:40:21 +0200

    mailto added

--- page-bottom.php
+++ page-bottom.php
@@ -4,7 +4,7 @@
       <h2>Skontaktuj się z nami:</h2>
     </header>
     <p class="contact phone">+48 111 222 333</p>
-    <p class="contact mail">piotr@example.com</p>
+    <p class="contact mail"><a href="mailto: piotr@example.com" title="Napisz do nas maila!">piotr@example.com</a></p>
     <h2>Znajdź nas na mapie:</h2>
     <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10232.168524922956!2d19.94883764602662!3d50.12293357836606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716508455927c7b%3A0xf882512547ab7c!2s32-087+Bibice!5e0!3m2!1spl!2spl!4v1531743478605" frameborder="0" style="border:0;" allowfullscreen></iframe>
     <aside class="page-author">
