a33d15af8a6341cdb7b6f30d3c11134c9aeb325e
Ajver <zawlockidominik@gmail.com>
2018-07-17 22:59:02 +0200

    Little semantic change

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php include('page-top.php') ?>
-	<link rel="stylesheet" href="css/offer.css">
-	
-	<h1>Kontakt</h1>
+  <link rel="stylesheet" href="css/offer.css">
 
+  <header>
+    <h1>Kontakt</h1>
+  </header>
 <?php include('page-bottom.php') ?>
