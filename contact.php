604cdff6c4c26debfeaf8645f119640de5643ac0
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2018-08-20 21:16:14 +0200

    Total rewrite

--- contact.php
+++
@@ -1,7 +0,0 @@
-<?php include('page-top.php') ?>
-  <link rel="stylesheet" href="css/contact.css?2">
-
-  <header>
-    <h1>Kontakt</h1>
-  </header>
-<?php include('page-bottom.php') ?>
--- index.php
+++ index.php
@@ -1,9 +1,89 @@
-<?php include('page-top.php') ?>
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+  <meta charset="UTF-8">
+  <title>O nas - Haussystem</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700&amp;subset=latin-ext" rel="stylesheet">
+  <link rel="shortcut icon" href="img/icon.png?1">
+  <link rel="stylesheet" href="css/style.css?2">
+</head>
+
+<body>
+  <header class="page-header">
+    <div class="logo"><img src="img/HausSystem-logo orange.png" alt="Haussystem"></div>
+    <div class="hamburger">
+      <div class="hamburger-block top"></div>
+      <div class="hamburger-block middle"></div>
+      <div class="hamburger-block bottom"></div>
+    </div>
+    <nav class="main-nav">
+      <a title="Poczytaj czym się zajmujemy" class="menu-btn about-us-btn" href="haussystem-o-nas">O nas</a>
+      <a title="Poznaj naszą ofertę" class="menu-btn offer-btn" href="nasza-oferta">Oferta</a>
+      <a title="Skontaktuj się z nami" class="menu-btn contact-btn" href="kontakt-do-nas">Kontakt</a>
+    </nav>
+  </header>
+  <main>
    
     <header>
       <h1>O nas</h1>
     </header>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, beatae a! Ipsam explicabo deleniti harum, similique doloribus quam atque. A eligendi, harum laborum esse ipsum nihil adipisci vel sequi voluptate!</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, beatae a! Ipsam explicabo deleniti harum, similique doloribus quam atque. A eligendi, harum laborum esse ipsum nihil adipisci vel sequi voluptate!</p>
+    
+    <header>
+  <h1>Oferta</h1>
+		</header>
 
-<?php include('page-bottom.php') ?>
+		<div class="one-offer">
+			<header>
+				Pierwsza oferta
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut voluptates blanditiis dolorum in, ipsum ratione iusto corrupti et suscipit asperiores nobis nihil dolor totam enim at unde? Eveniet, iure, accusantium.</p>
+		</div>
+
+		<div class="one-offer">
+			<header>
+				Druga oferta
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut voluptates blanditiis dolorum in, ipsum ratione iusto corrupti et suscipit asperiores nobis nihil dolor totam enim at unde? Eveniet, iure, accusantium.</p>
+		</div>
+
+		<div class="one-offer">
+			<header>
+				Trzecia oferta
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut voluptates blanditiis dolorum in, ipsum ratione iusto corrupti et suscipit asperiores nobis nihil dolor totam enim at unde? Eveniet, iure, accusantium.</p>
+		</div>
+
+		<div class="one-offer">
+			<header>
+				Czwarta oferta
+			</header>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut voluptates blanditiis dolorum in, ipsum ratione iusto corrupti et suscipit asperiores nobis nihil dolor totam enim at unde? Eveniet, iure, accusantium.</p>
+		</div>
+
+ </main>
+  <footer class="page-footer">
+    <header>
+      <h2>Skontaktuj się z nami:</h2>
+    </header>
+    <p class="contact phone">+48 111 222 333</p>
+    <p class="contact mail"><a href="mailto: piotr@example.com" title="Napisz do nas maila!">piotr@example.com</a></p>
+    <h2>Znajdź nas na mapie:</h2>
+    <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10232.168524922956!2d19.94883764602662!3d50.12293357836606!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4716508455927c7b%3A0xf882512547ab7c!2s32-087+Bibice!5e0!3m2!1spl!2spl!4v1531743478605" frameborder="0" style="border:0;" allowfullscreen></iframe>
+    <aside class="page-author">
+      <p>Made by <a href="#">Zawlocki and Pałka</a></p>
+    </aside>
+  </footer>
+
+  <script src="js/main.js?6" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script src="js/jquery.scrollTo.min.js" type="text/javascript"></script>
+
+</body>
+
+</html>
