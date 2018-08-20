6af6680ed4bee2119215470ebf8fdc21a9ddcb9c
Szymon1111 <40290583+Szymon1111@users.noreply.github.com>
2018-08-20 22:37:14 +0200

    ...

--- index.php
+++ index.php
@@ -89,10 +89,6 @@
   <script src="js/main.js?6" type="text/javascript"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="js/jquery.scrollTo.min.js" type="text/javascript"></script>
-	<script src="js/wow.min.js"></script>
-	<script>
-	new WOW().init();
-	</script>
 
 </body>
 
