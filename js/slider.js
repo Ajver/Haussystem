f7df205c00544a29a60136dd830123b71bdd1641
Ajver <zawlockidominik@gmail.com>
2018-11-10 22:25:33 +0100

    slaider still in work...

--- js/slider.js
+++ js/slider.js
@@ -20,31 +20,30 @@ const mainNavSticky = () => {
     document.querySelector(".page-header .slider").style.top = ScrollY/2 + "px";
 }
 
-var slideIndex = 0;
-
-var nextSlide = function(className, isTimeout) {  
+var nextSlide = function(className, isTimeout, si) {  
   var slides = document.getElementsByClassName(className);
-  var curSlide = slides[slideIndex];
+  console.log(slides);
+  console.log('SI: ' + si);
+  var curSlide = slides[si];
   
   // Removing class from last slide 
-  if(slideIndex == 0) { 
+  if(si == 0) { 
     slides[slides.length-1].classList.remove("active");
   }else {
-    slides[slideIndex-1].classList.remove("active");
+    slides[si-1].classList.remove("active");
   }
   
   // Setting class for active slide
   curSlide.classList.add("active");
   
-  // Next slide index
-  slideIndex = (slideIndex+1) % slides.length;
-  
   if(isTimeout) {
-    window.setTimeout(nextSlide, 6000, className, isTimeout);
+    window.setTimeout(() => {
+        nextSlide(className, isTimeout, (si+1) % slides.length);
+    }, 6000);
   }
 }
 
-nextSlide();
+nextSlide('header-slider-img', true, 0);
 
 var prevSlide = function(className) {  
     var slides = document.getElementsByClassName(className);
@@ -64,10 +63,13 @@ var prevSlide = function(className) {
     slideIndex = (slideIndex+1) % slides.length;
 }
 
+let slideIndex = 0;
+
 document.querySelector('.previous-slide-btn').addEventListener('click', () => {
-    prevSlide('slider-img', false);
+    --slideIndex;
+    prevSlide('slider-img');
 });
 
 document.querySelector('.next-slide-btn').addEventListener('click', () => {
-    nextSlide('slider-img');
+    nextSlide('slider-img', false, ++slideIndex);
 });
